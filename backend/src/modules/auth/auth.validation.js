9789265029d13d8de9ace39f6d0d8c236d26ad39
gurdeepsdev <gurdeep@clickorbits.com>
2026-06-26 14:34:40 +0530

    feat(core): implement validation framework

---
+++ backend/src/modules/auth/auth.validation.js
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+import Common from "../../shared/validators/common.validators.js";
+
+export const sendOtpSchema = z.object({
+
+    body: z.object({
+
+        provider: z.enum(["phone", "email"]),
+
+        identifier: z.union([
+
+            Common.phone,
+
+            Common.email
+
+        ])
+
+    })
+
+});
+
+export const verifyOtpSchema = z.object({
+
+    body: z.object({
+
+        provider: z.enum(["phone", "email"]),
+
+        identifier: z.union([
+
+            Common.phone,
+
+            Common.email
+
+        ]),
+
+        otp: Common.otp
+
+    })
+
+});
--- backend/src/routes/test.routes.js
+++ backend/src/routes/test.routes.js
@@ -2,6 +2,12 @@ import { Router } from "express";
 
 import InvalidOtpError from "../core/errors/auth/invalid-otp.error.js";
 
+import validate from "../middleware/validate.middleware.js";
+
+import { sendOtpSchema } from "../modules/auth/auth.validation.js";
+
+import Response from "../core/response/index.js";
+
 const router = Router();
 
 router.get("/error", (req, res, next) => {
@@ -9,5 +15,23 @@ router.get("/error", (req, res, next) => {
     next(new InvalidOtpError());
 
 });
+
+router.post(
+    "/validation",
+    validate(sendOtpSchema),
+    (req, res) => {
+
+        return Response.success(
+
+            res,
+
+            req.validated,
+
+            "Validation Passed"
+
+        );
+
+    }
+);
 
 export default router;
